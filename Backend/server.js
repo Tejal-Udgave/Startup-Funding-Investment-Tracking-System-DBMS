719b466038c112a68aaa1c7ead2270d7abef679f
Tejal Udgave <trudgave@gmail.com>
2026-06-05 03:39:07 +0530

    Add Aiven port and SSL

--- Backend/server.js
+++ Backend/server.js
@@ -12,9 +12,13 @@ app.use(express.json());
 // ================= DB =================
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "root123",
   database: process.env.DB_NAME || "SFITS_DBMS_PRJ",
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 db.connect((err) => {
